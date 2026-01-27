01e2308a346102bd4703e5478bf7f9e761be78d3
krishaparamesvaran <krishaparameshvaran1609@gmail.com>
2026-01-27 10:23:20 +0530

    Add files via upload

---
+++ java/sumandavg.java
@@ -0,0 +1,23 @@
+package pst;
+import java.util.Scanner;
+
+public class sumandavg {
+	
+	public static void main(String[] args) {
+
+       Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int sum = a + b + c;
+        double avg = sum / 3.0;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Avg = " + avg);
+    }
+}
+
+	
+	        
